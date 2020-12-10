b5059022bd04c47f5581c9a6248412cf792f037d
Isvvc <lyons@tuta.io>
2020-12-09 17:45:32 -0700

    Create basic network request

--- Single File PHP Gallery Downloader/Single File PHP Gallery Downloader/main.swift
+++ Single File PHP Gallery Downloader/Single File PHP Gallery Downloader/main.swift
@@ -6,6 +6,43 @@
 //
 
 import Foundation
+import ArgumentParser
 
-print("Hello, World!")
+enum SFPHPGDError: Error {
+    case invalidURL
+}
 
+struct SFPHPGD: ParsableCommand {
+    @Flag(help: "Show verbose printout.")
+    var verbose = false
+
+    @Option(name: .shortAndLong, help: "Maximum depth of folders to traverse.")
+    var maxDepth: Int?
+
+    @Argument(help: "The URL of the Single File PHP Gallery.")
+    var url: String
+
+    mutating func run() throws {
+        guard let url = URL(string: url) else {
+            throw SFPHPGDError.invalidURL
+        }
+        
+        let semaphor = DispatchSemaphore(value: 0)
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil,
+                  let data = data,
+                  let html = String(data: data, encoding: .utf8) else {
+                semaphor.signal()
+                return
+            }
+            
+            print(html)
+            semaphor.signal()
+        }.resume()
+        
+        semaphor.wait()
+    }
+}
+
+SFPHPGD.main()
